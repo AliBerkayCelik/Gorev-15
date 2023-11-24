0b633c0e97a7a0e38b62af388ba5990657e42a76
Ali Berkay Celik <aliberkaycelik@hotmail.com>
2023-11-24 21:54:12 +0300

    degisiklikler eklendi

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./src/features/home/HomeScreen";
+import CustomerScreen from "./src/features/home/CustomerScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LocationScreen from "./src/features/home/LocationScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        
+          name="Customer"
+          component={CustomerScreen}
+          options={
+            {
+              title: 'Müşteri ol',
+              headerStyle:{backgroundColor:'#96234f'},
+              headerTintColor:'#fff',
+              headerTitleAlign:'center'
+            }
+            
+          }
+          
+        />
+        <Stack.Screen
+          name="Location"
+          component={LocationScreen}
+          options={
+            { 
+              title: 'Konum Bilgisi',
+              headerStyle:{backgroundColor:'#96234f'},
+              headerTintColor:'#fff',
+              headerTitleAlign:'center'
+            }
+          }
+        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
